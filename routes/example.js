58ef3ac5da57090f89f1541ddbd94c9dd76874c1
Vinoth <agentangel97@gmail.com>
2019-11-20 20:10:14 +0530

    adds Joi validator

--- routes/example.js
+++ routes/example.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const Joi = require("@hapi/joi");
 
 class ExampleRoutes {
   constructor(controller) {
@@ -10,6 +11,26 @@ class ExampleRoutes {
   init() {
     router.post("/login", async (req, res) => {
       try {
+        const schema = {
+          username: Joi.string().required(),
+          password: Joi.string().required()
+        };
+
+        const username = req.body.username;
+        const password = req.body.password;
+
+        const isValid = Joi.validate(
+          {
+            username: username,
+            password: password
+          },
+          schema
+        );
+
+        if (isValid.error !== null) {
+          throw isValid.error;
+        }
+
         const getUserDetails = await this.controller.login(
           req.body.username,
           req.body.password
@@ -17,8 +38,12 @@ class ExampleRoutes {
 
         res.json(getUserDetails);
       } catch (err) {
-        global.log.error(err);
-        res.json({ code: 500, msg: "An error occurred !" });
+        if (err.name === "ValidationError") {
+          res.json({ code: 422, msg: err.details[0].message });
+        } else {
+          global.log.error(err);
+          res.json({ code: 500, msg: "An error occurred !" });
+        }
       }
 
       res.end();
